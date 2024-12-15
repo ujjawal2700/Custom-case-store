379530afca63d1f54262c258e50c8d70baee7b1e
Ujjawal Kumar Mahawar <90878964+ujjawal2700@users.noreply.github.com>
2024-12-16 04:21:25 +0530

    finally project is in working condition

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -7,7 +7,7 @@ import Stripe from "stripe";
 async function POST(req: Request){
   try{
     const body = await req.text();
-    const signature = headers().get("stripe-signature")
+    const signature = (await headers()).get("stripe-signature")
 
     if(!signature){
       return new Response("Invalid signature", {status: 400})
